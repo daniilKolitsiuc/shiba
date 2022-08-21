1613dc1991d6878d651d19fa1fdac221fe721ed7
daniilKolitsiuc <dospodborka@gmail.com>
2022-08-21 23:48:46 +0300

    Update ShibaWeb.UE4.js

--- ShibaWeb.UE4.js
+++ ShibaWeb.UE4.js
@@ -1154,7 +1154,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MORE JS
-		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('ShibaWeb.data.js'));
+		var dataJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/daniilKolitsiuc/shiba/6d55d7406b8c60f854e71e661470b6b64fb1be2f/ShibaWeb.data.js'));
 		var utilityJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('Utility.js')).then(addScriptToDom);
 		var dataDownload =
 /* // The following code would download and store the .data file as a Blob, which should be more efficient than loading an ArrayBuffer. However that seems to be buggy, so avoid it for now.
